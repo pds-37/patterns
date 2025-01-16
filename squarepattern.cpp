7a4269d7db0faaa50f64fc57e89c9e34f981d73c
Priyanshu tiwari <official.priyanshu.37@gmail.com>
2025-01-16 23:14:37 +0530

    Update squarepattern.cpp

--- squarepattern.cpp
+++ squarepattern.cpp
@@ -1,19 +1,41 @@
+/* To Print the given pattern
+
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5     
+
+ */ 
+
+
+
 #include <iostream>
 using namespace std;
 
-void printPattern(int n) {
-    int size = 2 * n - 1;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int min =({i, j, size - i - 1, size - j - 1});
-            cout << (n - min) << " ";
+int sqrpattern(int n){
+    
+    for (int i = 0; i < 2 * n - 1; i++) 
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+         {
+
+            int minD = min(min(i, j), min(2 * n - 2 - i, 2 * n - 2 - j));
+            cout << n - minD << " ";
         }
         cout << endl;
     }
+
 }
 
 int main() {
-    int n = 5; // Change this to adjust the size of the pattern
-    printPattern(n);
+    int n;
+    cout<<"Enter the number of rows:";
+    cin>>n;
+    sqrpattern(n);
     return 0;
 }
